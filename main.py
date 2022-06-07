610ca45e29776b47249246ec1bc9b2d329a9eaea
V0rt3x69 <105158732+V0rt3x69@users.noreply.github.com>
2022-06-07 10:05:11 +0400

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ port = 587
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-gmail = ""  # Here I declared a few variables for the emails
+gmail = ""
 email1 = ""
 email2 = ""
 email3 = ""
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-password = ""  # Here I declared some variables for the passwords
+password = ""
 password1 = ""
 password2 = ""
 password3 = ""
@@ -24,49 +24,41 @@ password3 = ""
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 print("Welcome to the Gmail Spam Bot")
-howManyEmails = input(     # This is a variable that uses the input command which can let you pick between the 3 options
+howManyEmails = input(
     "Please write [1] if you want to use 1 email, write [2], if you want to use 2 and [3] if you want to use 3... ")
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# This is what would happen if you wrote 1 for the answer above
 if howManyEmails == "1" or howManyEmails == "[1]":
 
-    # This is where it asks for your email
     email1 = input("Please enter the email you want to use... ")
     password1 = input(
-        "Please enter the password for the email that you want to use... ")  # This is where it asks you for your password
+        "Please enter the password for the email that you want to use... ")
 
     reciever = input(
-        "Please write the email of the person that you want to recieve the emails... ")  # This is where it asks you who is recieving the emails
+        "Please write the email of the person that you want to recieve the emails... ")
 
-    # This is what to send to the email
     what_to_send = input("What do you want to send... ")
 
-    # This changes all email variables to email 1 (This was for bug fixing)
     emails = email1
     email2 = email1
     email3 = email1
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# This is what would happen if you wrote 2 above
 elif howManyEmails == "2" or howManyEmails == "[2]":
 
-    # This is the first email
     email1 = input("Please enter the first email that you want to use... ")
     password1 = input(
-        "Please enter the password for the first email that you want to use... ")  # This is the first password
+        "Please enter the password for the first email that you want to use... ")
 
-    # This is the second email
     email2 = input("Please enter the second email that you want to use... ")
     password2 = input(
-        "Please enter the password for the second email that you want to use... ")  # This is the second password
+        "Please enter the password for the second email that you want to use... ")
 
     reciever = input(
-        "Please write the email of the person that you want to recieve the emails... ")  # This is who recieves the emails
+        "Please write the email of the person that you want to recieve the emails... ")
 
-    # This is what to send to the email
     what_to_send = input("What do you want to send... ")
 
     emails = [email1, email2]
@@ -75,48 +67,43 @@ elif howManyEmails == "2" or howManyEmails == "[2]":
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# This is what would happen if you wrote 3 above
 elif howManyEmails == "3" or howManyEmails == "[3]":
 
-    # This is the first email
     email1 = input("Please enter the first email that you want to use... ")
     password1 = input(
-        "Please enter the password for the first email that you want to use... ")  # This is the first password
+        "Please enter the password for the first email that you want to use... ")
 
-    # This is the second email
     email2 = input("Please enter the second email that you want to use... ")
     password2 = input(
-        "Please enter the password for the second email that you want to use... ")  # This is the second password
+        "Please enter the password for the second email that you want to use... ")
 
-    # This is the third email
     email3 = input("Please enter the third email that you want to use... ")
     password3 = input(
-        "Please enter the password for the third email that you want to use... ")  # This is the third password
+        "Please enter the password for the third email that you want to use... ")
 
     reciever = input(
-        "Please write the email of the person that you want to recieve the emails... ")  # This is who recieves the email
+        "Please write the email of the person that you want to recieve the emails... ")
 
-    # This is what you want to send
     what_to_send = input("What do you want to send... ")
 
     emails = [email1, email2, email3]
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-else:  # This is what would happen if you didnt write [1] [2] or [3]
+else:
     print("You didnt write [1] [2] or [3]")
     quit()
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-for_while = input(  # This is whether you want to send a limited or unlimited amound of emails
+for_while = input(
     "Write [1] if you want to only send a limited amount of messages and write [2] if you want to write an unlimited amound of messages... ")
 
 amount_of_times_for_loop = 0
 start_while_loop = False
 
-# This is what happenes if you wanted a limited amound
+
 if for_while == "1" or for_while == "[1]":
     amount_of_times_for_loop = input(
         "How many times do you want it to repeat? ")
@@ -126,20 +113,18 @@ if for_while == "1" or for_while == "[1]":
     sleep_wait = int(sleep_wait)
 
 
-# This is what happenes if you want an unlimited amound
 if for_while == "2" or for_while == "[2]":
     sleep_wait = input(
         "How long do you want to wait between each message (in seconds, minimum 1)? ")
     sleep_wait = int(sleep_wait)
     start_while_loop = True
 
-if sleep_wait < 1:
+if sleep_wait < 0.1:
     print("An error has occured")
     quit()
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# This is a for loop for what would happen if you wanted a limited amound
 for i in range(0, amount_of_times_for_loop):
     gmail1 = random.randint(0, 2)
 
@@ -171,7 +156,7 @@ for i in range(0, amount_of_times_for_loop):
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    server = "smtp.gmail.com"  # This is the gmail bot for the for loop
+    server = "smtp.gmail.com"
 
     context = ssl.create_default_context()
 
@@ -190,7 +175,7 @@ for i in range(0, amount_of_times_for_loop):
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-while start_while_loop == True:  # This is would would happen if you wanted an unlimited amound of emails
+while start_while_loop == True:
     gmail1 = random.randint(0, 2)
 
     if gmail1 == 0:
@@ -220,7 +205,7 @@ while start_while_loop == True:  # This is would would happen if you wanted an u
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    server = "smtp.gmail.com"  # This is the gmail bot for the while loop
+    server = "smtp.gmail.com"
 
     context = ssl.create_default_context()
 
